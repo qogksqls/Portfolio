0211c9caf77ccebbd7887c8898d561dbc447abea
qogksqls <qoqls1212@gmail.com>
2023-10-08 17:52:43 +0900

    test

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -2,7 +2,11 @@ import React from "react";
 import "./Main.scss";
 
 const Main = () => {
-  return <div className="mainpage">BAE HAN BEEN</div>;
+  return <div className="mainpage">
+    BAE HAN BEEN
+
+    hi
+    </div>;
 };
 
 export default Main;
